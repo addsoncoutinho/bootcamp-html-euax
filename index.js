28ac8f7825d6c2ae528662884148984a3050c1cf
addsoncoutinho <addson.coutinho@euax.com.br>
2022-04-05 13:50:51 -0300

    funcao de submit

---
+++ index.js
@@ -0,0 +1,7 @@
+const teste = () => {
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const area = document.querySelector('#area').value;
+    
+    return alert(`wooooooooooooow \n \n Seja bem-vindo ${name} ao bootcamp da àrea de ${area}. \n \n Você se cadastrou com o e-mail ${email}.`)
+}
